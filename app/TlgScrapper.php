55f630575cd7847ace2a38743ec2088b59370135
faryar76 <faryar76@hotmail.com>
2019-06-02 15:40:17 +0430

    add ability to parse messages

--- app/TlgScrapper.php
+++ app/TlgScrapper.php
@@ -1,25 +1,34 @@
 <?php
 
 namespace Faryar76;
-
+use GuzzleHttp\Client;
 class TlgScrapper
 {
     protected $rawContent;
     protected $client;
     protected $patterns=[
         'name'=>'/<div class="tgme_header_title">(.*?)<\/div>/is',
-        'count_memmber'=>'/<span class="counter_value">(.*?)<\/span> <span class="counter_type">members<\/span>/is'
+        'count_memmber'=>'/<span class="counter_value">(.*?)<\/span> <span class="counter_type">members<\/span>/is',
+        'count_link'=>'/videos.*?<span class="counter_value">(.*?)<\/span> <span class="counter_type">links<\/span>/is',
+        'count_video'=>'/photos.*?<span class="counter_value">(.*?)<\/span> <span class="counter_type">videos<\/span>/is',
+        'count_photo'=>'/members.*?<span class="counter_value">(.*?)<\/span> <span class="counter_type">photos<\/span>/is',
+        'description'=>'/  <meta name="twitter:description" content="(.*?)">/is',
+        'bubbles'=>'/(<div class="tgme_widget_message_wrap js-widget_message_wrap.*?">.*?datetime.*?<\/div>)/ms'
     ];
-    public function __construct()
+    public function __construct($httpClient=null)
     {
-        $this->client=new Guzzle();
+        if(!$httpClient)
+        {
+            $httpClient=new Client();
+        }
+        $this->client=$httpClient;
         $this->normalizer=new Normalizer();
     }
     public function load($username)
     {
         $username=$this->normalizer->username($username);
         $url=sprintf('https://t.me/s/%s',$username);
-        $this->rawContent=$this->client->get($url);
+        $this->rawContent=$this->client->get($url)->getBody()->getContents();
     }
     public function getName()
     {
@@ -29,9 +38,55 @@ class TlgScrapper
     {
         return $this->preg('count_memmber')[1];
     }
+    public function getVideoCount()
+    {
+        return $this->preg('count_video')[1];
+    }
+    public function getDescription()
+    {
+        return str_replace("\n",'',$this->preg('description')[1]);
+    }
+    public function getLinkCount()
+    {
+        return $this->preg('count_link')[1];
+    }
+    public function getPhotoCount()
+    {
+        return $this->preg('count_photo')[1];
+    }
+    public function getMessages()
+    {
+        preg_match_all($this->patterns['bubbles'],$this->rawContent,$matches);
+        foreach ($matches[1] as $key => $value) {
+            $result[]=$this->parseMessage($value);
+        }
+        return new Collection($result);
+    }
     public function preg($key)
     {
-        preg_match($this->patterns[$key],$this->rawContent->getContents(),$matches);
+        preg_match($this->patterns[$key],$this->rawContent,$matches);
         return $matches; 
+    }
+    public function parseMessage($message)
+    {
+        $result=[];
+        // id
+        $id='/class="tgme_widget_message_date".*?href="https:\/\/t\.me\/telegram\/([0-9]+)/ism';
+        preg_match($id,$message,$res);
+        $result['id']=(int) $res[1];
+        // get date
+        $date='/<time datetime="(.*?)">[0-9:]+<\/time>/is';
+        preg_match($date,$message,$res);
+        $result['date']['date']=$res[1];
+        $result['date']['unix']=strtotime($res[1]);
+        // get viws
+        $view='/class="tgme_widget_message_views">(.*?)<\/span>/ms';
+        preg_match($view,$message,$res);
+        $result['view']=$res[1] ?? "";
+        // text
+        $text='/class="tgme_widget_message_text js-message_text".*?>(.*?)<\/div>/is';
+        preg_match($text,$message,$res);
+        $result['text']=strip_tags(trim(preg_replace('/\s\s+/', ' ', html_entity_decode($res[1]??"",ENT_QUOTES))));
+        return $result;
     }
 }
--- tests/TlgScrapperTest.php
+++ tests/TlgScrapperTest.php
@@ -2,24 +2,88 @@
 
 use PHPUnit\Framework\TestCase;
 use Faryar76\TlgScrapper;
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use Faryar76\Collection;
 /**
  * ClassNameTest
  * @group group
  */
 class TlgScrapperTest extends TestCase
 {
+    public $main;
+    protected function setUp():void
+    {
+        $mock = new MockHandler([
+            new Response(200,[],file_get_contents(__DIR__."/file")),
+        ]);
+        
+        $handler = HandlerStack::create($mock);
+        $client = new Client(['handler' => $handler]);
+        $this->main=new TlgScrapper($client=$client);
+    }
     /** @test */
     public function test_can_get_name_of_channel()
     {
-        $main=new TlgScrapper;
-        $main->load('@telegram');
-        $this->assertEquals('Telegram News',$main->getName());
+        $this->main->load('@telegram');
+        $this->assertEquals('Telegram News',$this->main->getName());
     }
     /** @test */
     public function test_can_get_channel_memmer_count()
     {
-        $main=new TlgScrapper;
-        $main->load('@telegram');
-        $this->assertEquals('2.8M',$main->getMemmberCount());
+        $this->main->load('@telegram');
+        $this->assertEquals('2.8M',$this->main->getMemmberCount());
+    }
+    /** @test */
+    public function test_can_get_channel_video_count()
+    {
+        $this->main->load('@telegram');
+        $this->assertEquals('3',$this->main->getVideoCount());
+    }
+    /** @test */
+    public function test_can_get_channel_link_count()
+    {
+        $this->main->load('@telegram');
+        $this->assertEquals('56',$this->main->getLinkCount());
+    }
+    /** @test */
+    public function test_can_get_channel_photo_count()
+    {
+        $this->main->load('@telegram');
+        $this->assertEquals('6',$this->main->getPhotoCount());
+    }
+    /** @test */
+    public function test_can_get_channel_description()
+    {
+        $this->main->load('@telegram');
+        $this->assertEquals('The official Telegram on Telegram. Much recursion. Very Telegram. Wow.',$this->main->getDescription());
+    }
+    /** @test */
+    public function test_must_get_page_messages_as_a_collection()
+    {
+        $this->main->load('@telegram');
+        $this->assertInstanceOf(Collection::class,$this->main->getMessages());
+    }
+    /** @test */
+    public function test_must_get_page_messages()
+    {
+        $this->main->load('@telegram');
+        $this->assertCount(20,$this->main->getMessages());
+    }
+    /**
+     * @test
+     */
+    public function test_must_return_parsed_bubble()
+    {
+        $this->main->load('@telegram');
+        $message=$this->main->getMessages()[0];
+        $this->assertEquals('2018-02-01T18:09:24+00:00',$message['date']['date']);
+        $this->assertEquals(1517508564,$message['date']['unix']);
+        $this->assertEquals('3.7M',$message['view']);
+        $this->assertEquals("Here's a video demo to give you a taste of Telegram X for Android.",$message['text']);
+        $this->assertEquals(90,$message['id']);
+        print_r($this->main->getMessages());
     }
 }
