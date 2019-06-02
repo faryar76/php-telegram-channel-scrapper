5093e365a957e38d588c44af3901720861a4c117
faryar76 <faryar76@hotmail.com>
2019-06-02 19:41:36 +0430

    add helpers

--- app/Collection.php
+++ app/Collection.php
@@ -4,4 +4,17 @@ use arrayObject;
 class Collection extends arrayObject
 {
     
+    public function toArray()
+    {
+        return $this->getArrayCopy();
+    }
+    public function first()
+    {
+        return current($this->getArrayCopy());
+    }
+    public function latest()
+    {
+        $data=$this->getArrayCopy();
+        return end($data);
+    }
 }
